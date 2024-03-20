7391d847b818c50ecbff5464aedbab6d8dc3704b
Philipp Middendorf <philipp.middendorf@desy.de>
2024-03-20 08:03:07 +0100

    Update dependencies and use unliftio

--- app/P11RunnerHs.hs
+++ app/P11RunnerHs.hs
@@ -7,10 +7,9 @@ import Control.Monad.IO.Class (MonadIO, liftIO)
 import Data.Text (Text, pack, unpack)
 import Data.Text.IO (putStrLn)
 import Foreign.C (CInt, CLong)
-import Foreign.C.String (CString, newCString, peekCString, withCString)
+import Foreign.C.String (CString, newCString)
 import Foreign.Marshal (free, peekArray, with)
 import Foreign.Marshal.Alloc (free, malloc)
-import Foreign.Marshal.Array (withArray)
 import Foreign.Marshal.Utils (new)
 import Foreign.Ptr (FunPtr, Ptr, castPtr, nullPtr)
 import Foreign.Storable (peek, poke)
@@ -34,6 +33,8 @@ import Tango.Server
     tango_server_set_status,
     tango_server_start,
   )
+import qualified UnliftIO
+import UnliftIO.Foreign (peekCString, withArray, withCString)
 import Prelude hiding (putStrLn)
 
 data InitedServer = InitedServer
@@ -42,14 +43,19 @@ newtype PropertyName = PropertyName {getPropertyName :: Text}
 
 newtype ServerStatus = ServerStatus Text
 
-withCStringFromText :: Text -> (CString -> IO a) -> IO a
+withCStringFromText :: (UnliftIO.MonadUnliftIO m) => Text -> (CString -> m a) -> m a
 withCStringFromText t = withCString (unpack t)
 
-tangoServerInit :: [PropertyName] -> ServerStatus -> HaskellTangoDevState -> IO (Either Text InitedServer)
+tangoServerInit ::
+  (UnliftIO.MonadUnliftIO m) =>
+  [PropertyName] ->
+  ServerStatus ->
+  HaskellTangoDevState ->
+  m (Either Text InitedServer)
 tangoServerInit propertyNames (ServerStatus initialStatus) initialState = do
-  progName <- getProgName
-  args <- getArgs
-  freeFinalizerWrapped <- createGlobalFinalizer free
+  progName <- liftIO getProgName
+  args <- liftIO getArgs
+  freeFinalizerWrapped <- liftIO (createGlobalFinalizer free)
   case args of
     [] -> pure (Left "cannot initialize device server, missing first argument (instance name)")
     (instanceName : _) ->
@@ -58,22 +64,23 @@ tangoServerInit propertyNames (ServerStatus initialStatus) initialState = do
           withArray [progNameC', instanceNameC'] \programArgumentsC ->
             withCString (unpack initialStatus) \initialStatusC -> do
               mapM_
-                (\propName -> withCString propName tango_server_add_property)
+                (\propName -> withCString propName (\x -> liftIO (tango_server_add_property x)))
                 ((unpack . getPropertyName) <$> propertyNames)
-              tango_server_init
-                2
-                programArgumentsC
-                freeFinalizerWrapped
-                initialStatusC
-                (fromIntegral $ fromEnum initialState)
+              liftIO $
+                tango_server_init
+                  2
+                  programArgumentsC
+                  freeFinalizerWrapped
+                  initialStatusC
+                  (fromIntegral $ fromEnum initialState)
               pure (Right InitedServer)
 
-tangoServerStart :: InitedServer -> IO ()
-tangoServerStart _initedServer = tango_server_start
+tangoServerStart :: (UnliftIO.MonadUnliftIO m) => InitedServer -> m ()
+tangoServerStart _initedServer = liftIO tango_server_start
 
-tangoReadProperty :: InitedServer -> PropertyName -> IO Text
+tangoReadProperty :: (UnliftIO.MonadUnliftIO m) => InitedServer -> PropertyName -> m Text
 tangoReadProperty _initedServer (PropertyName n) = do
-  resultAsCString <- withCStringFromText n tango_server_read_property
+  resultAsCString <- withCStringFromText n (\x -> liftIO (tango_server_read_property x))
   resultAsString <- peekCString resultAsCString
   pure (pack resultAsString)
 
